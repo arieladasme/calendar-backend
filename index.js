f328aaf3cd482ee8b06fb926406e183c6a3d3492
ariel <ariel.wm1@gmail.com>
2023-01-10 16:08:21 -0300

    adding public/ & .env

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express')
+require('dotenv').config()
 
 // creando servidor express
 const app = express()
 
+// directorio publico
+app.use(express.static('public'))
+
 // rutas
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.json({
     ok: true,
   })
-})
+}) */
 
 // escuchar peticiones
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
