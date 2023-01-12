2917457505b491b63e283f1d69409a78266a9dcb
ariel <ariel.wm1@gmail.com>
2023-01-12 13:33:39 -0300

    custom middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,23 @@
 const { response } = require('express')
-const { validationResult } = require('express-validator')
 
 const createUser = (req, res = response) => {
   const { name, email, password } = req.body
-
-  // errors
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    })
-  }
-
   res.status(201).json({ register: true, name, email, password })
 }
 
 const loginUser = (req, res = response) => {
   const { email, password } = req.body
-
-  // errors
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    })
-  }
-
-  res.json({ login: true, email, password })
+  res.json({
+    login: true,
+    email,
+    password,
+  })
 }
 
 const renewToken = (req, res = response) => {
-  res.json({ renew: true })
+  res.json({
+    renew: true,
+  })
 }
 
 module.exports = { createUser, loginUser, renewToken }
--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 const router = Router()
 const { createUser, loginUser, renewToken } = require('../controllers/auth')
+const { fieldValidators } = require('../middlewares/field-validators')
 
 router.post(
   '/new',
@@ -17,6 +18,7 @@ router.post(
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe contener 6 caracteres').isLength({ min: 6 }),
+    fieldValidators,
   ],
   createUser
 )
@@ -27,6 +29,7 @@ router.post(
   [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe contener 6 caracteres').isLength({ min: 6 }),
+    fieldValidators,
   ],
   loginUser
 )
