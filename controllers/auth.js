7281282e27ff0b1cde826a81c5edfa63284defc5
ariel <ariel.wm1@gmail.com>
2023-01-12 14:40:30 -0300

    create schema user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,21 @@
 const { response } = require('express')
+const User = require('../models/User')
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
   const { name, email, password } = req.body
-  res.status(201).json({ register: true, name, email, password })
+
+  try {
+    const user = new User(req.body)
+    await user.save()
+
+    res.status(201).json({ register: true })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el admin',
+    })
+  }
 }
 
 const loginUser = (req, res = response) => {
