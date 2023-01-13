b25a5a5333615f069e6a94a8b02a7133b86eb467
ariel <ariel.wm1@gmail.com>
2023-01-13 18:11:40 -0300

    login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,8 +34,40 @@ const createUser = async (req, res = response) => {
   }
 }
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario no existe con ese correo',
+      })
+    }
+
+    const validPassword = await bcrypt.compareSync(password, user.password)
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Contraseña incorrecta',
+      })
+    }
+
+    //JWT validation
+    return res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el admin',
+    })
+  }
   res.json({
     login: true,
     email,
