d57adfd146a27d9115ebda8f5b60510c90c1ae1e
ariel <ariel.wm1@gmail.com>
2023-01-16 13:29:24 -0300

    create event

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express')
+const Event = require('../models/Event')
 
 const getEvents = (req, res = response) => {
   res.json({
@@ -7,11 +8,25 @@ const getEvents = (req, res = response) => {
   })
 }
 
-const createEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'createEvent',
-  })
+const createEvent = async (req, res = response) => {
+  const event = new Event(req.body)
+
+  try {
+    event.user = req.uid
+    const newEvent = await event.save()
+
+    res.json({
+      ok: true,
+      msg: 'createEvent',
+      event: newEvent,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el admin',
+    })
+  }
 }
 
 const updateEvent = (req, res = response) => {
--- models/Event.js
+++ models/Event.js
@@ -19,7 +19,15 @@ const EventSchema = Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
+    required: true,
   },
+})
+
+// reemplazo _id por id
+EventSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject()
+  object.id = _id
+  return object
 })
 
 module.exports = model('Event', EventSchema)
